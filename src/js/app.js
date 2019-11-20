3a39010aab21cae5f44f3cdbe7c0fba208424fbc
Monika Misiewicz <zieba.monika@wp.pl>
2019-11-20 21:37:21 +0100

    added bootstrap files, index.html, app.js

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+$(function(){
+    $("#booklist").on("click",function(){
+        $.ajax({
+            url:"http://localhost:8282/books/",
+            method:"get",
+            dataType:"json"
+        }).done(function(response){
+            console.log(response.results);
+            renderBooks(response.results)
+        }).fail(function(err){
+            console.log(err)
+        })
+    });
+
+    function renderBooks(books){
+        books.forEach(function(book){
+            $(".booklist").append($(`<li>${book.title}</li>`))
+        })
+    }
+
+});
+
+// $(".dropdown-item").on("click", function() {
+//
+// });
+
+// $(function(){
+//
+//
+// });
